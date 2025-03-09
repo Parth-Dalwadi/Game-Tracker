ab21c872ba40adb3de908564f960037715eacee3
Dalwadi <dalwadip77@gmail.com>
2025-03-09 15:25:37 -0500

    Creating UI for game list.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MyGameList.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGameList.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
