b461b789771563cc08b62e36f8f8e1409e21438f
Dalwadi <dalwadip77@gmail.com>
2025-03-09 17:05:57 -0500

    Finished setting up view change between two different pages.

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,36 @@ namespace MyGameList.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand TempViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public TempViewModel TempVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
         public MainViewModel()
         {
+            HomeVM = new HomeViewModel();
+            TempVM = new TempViewModel();
+            CurrentView = HomeVM;
 
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            TempViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TempVM;
+            });
         }
     }
 }
