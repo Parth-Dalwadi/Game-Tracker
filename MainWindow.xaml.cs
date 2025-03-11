0c47ac4dfd99f39379a6e5a463df3472cb66dd32
Dalwadi <dalwadip77@gmail.com>
2025-03-10 20:19:59 -0500

    Got successful API message.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Reflection;
+
 
 namespace MyGameList
 {
@@ -20,6 +26,8 @@ namespace MyGameList
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly HttpClient client = new HttpClient();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +38,26 @@ namespace MyGameList
             if (e.ChangedButton == MouseButton.Left)
             {
                 this.DragMove();
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var body = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("fields", "*;"),
+            };
+            client.DefaultRequestHeaders.Add("Client-ID", "FAKE-ID");
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer FAKE-TOKEN");
+            var content = new StringContent("fields *;", Encoding.UTF8, "application/json");
+            var response = client.PostAsync("https://api.igdb.com/v4/games", content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Trace.WriteLine("Success");
+                Trace.WriteLine(response);
+            } else
+            {
+                Trace.WriteLine("Fail");
             }
         }
     }
