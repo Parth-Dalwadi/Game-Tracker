e740701cc07e60c41eb3d4f76a2462829de9cea2
Dalwadi <dalwadip77@gmail.com>
2025-03-12 14:47:23 -0500

    Removed unnecessary code in Client.

--- API/Client/Client.cs
+++ API/Client/Client.cs
@@ -12,30 +12,5 @@ namespace MyGameList.API.Client
     class Client
     {
         protected HttpClient client = new HttpClient();
-
-        public async Task<Item[]> Get(int id)
-        {
-            return await Task.Run(() =>
-            {
-                var content = new StringContent("fields *;\nwhere id = " + id + ";", Encoding.UTF8, "application/json");
-                var response = client.PostAsync("https://api.igdb.com/v4/artworks", content).Result;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    Trace.WriteLine("Success");
-                    var jsonTask = response.Content.ReadAsStringAsync();
-                    jsonTask.Wait();
-                    var jsonStringResult = jsonTask.Result;
-                    JavaScriptSerializer js = new JavaScriptSerializer();
-                    Item[] items = js.Deserialize<Game[]>(jsonStringResult);
-                    return items;
-                }
-                else
-                {
-                    Item[] items = new Item[0];
-                    return items;
-                }
-            });
-        }
     }
 }
